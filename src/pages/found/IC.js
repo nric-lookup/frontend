63811a9e9a11036ecd609a5137bafa84c337756e
afrie <itsafrie@gmail.com>
2020-09-21 15:32:34 +0800

    [UX feature] Add form validation on IC component

--- src/pages/found/IC.js
+++ src/pages/found/IC.js
@@ -10,18 +10,19 @@ function IC({ hs1, e1, r1, onSubmit1, step, next }) {
   return (
     <>
       <form key={1} onSubmit={hs1(onSubmit1)} hidden={step !== 1}>
-        <p>Lost IC number</p>
+        <p>IC number that you found</p>
         <TextField
           fullWidth
+          autoFocus
           id='ic'
           name='ic'
           type='number'
           margin='normal'
           variant='outlined'
-          label='Enter IC number that you found'
+          label='IC number'
           error={e1.ic}
           helperText={e1.ic && 'Please provide a valid IC number'}
-          inputRef={r1()}
+          inputRef={r1({ minLength: 12, maxLength: 12, required: true })}
         />
 
         <Grid
@@ -36,7 +37,7 @@ function IC({ hs1, e1, r1, onSubmit1, step, next }) {
             variant='contained'
             color='primary'
             size='large'
-            onClick={() => next()}
+            onClick={() => !e1.ic && next()}
           >
             <Typography variant='subtitle1' component='subtitle1'>
               Next
