9d117ff81662dfbc1d39352f664ca859989b885b
afrie <itsafrie@gmail.com>
2020-09-23 19:36:32 +0800

    Update Thank You page

--- src/pages/found/ThankYou.js
+++ src/pages/found/ThankYou.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Typography from '@material-ui/core/Typography'
+
 function ThankYou({ step }) {
   return (
     <div hidden={step !== 5}>
-      <p>Thank You</p>
+      <Typography variant='h5' component='h5' align=''>
+        Thank You
+      </Typography>
+      <Typography variant='body2' component='p' align=''>
+        We appreciate your kindness. Please verify the claimer when they’ve
+        contacted you.
+      </Typography>
     </div>
   )
 }
